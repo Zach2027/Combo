89c22974eab8c40f897de10173a292b88f4542e4
Zach <zachary.candell27>
2024-10-21 12:09:20 -0700

    lot of prog

--- combo.java
+++ combo.java
@@ -1,5 +1,60 @@
-public class combo {
-    public static void main(String[] args) {
+import java.util.Scanner; // Import the Scanner class
 
+public class Combo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int jab = 0;
+        int cross = 0;
+        int slip = 0;
+        int kick = 0;
+        int power = 0;
+
+        System.out.println("A guy walks up to you and thows a punch, what would you like to do?");
+        System.out.println("1. Jab");
+        System.out.println("2. Cross");
+        System.out.println("3. Slip");
+
+        String firstChoice = scanner.nextLine();
+        if (firstChoice.equals("1")) {
+            System.out.println("You counter with a jab stunning your opponent");
+            jab += 1;
+        }
+        if (firstChoice == "3") {
+            System.out.println("You slip the punch giving you next shot more power");
+            slip += 2;
+        } else {
+            System.out.print("you were too slow and got hit in the head decressing future power");
+            slip -= 0.5;
+        }
+
+        System.out.println("for your next attack to finish you oppenent, you can ");
+        System.out.println("1. Jab");
+        System.out.println("2. Cross");
+        System.out.println("3. Kick");
+
+        String secondChoice = scanner.nextLine();
+        if (secondChoice == "1") {
+            jab += 1;
+            System.out.println("you jab was not enough to Ko, you must Strike again");
+            System.out.println("1. Jab");
+            System.out.println("2. Cross");
+            String secondChoiceB = scanner.nextLine();
+            if (secondChoiceB == "1") {
+                System.out.println("You land another jab knocking out you oponent in a clean manner");
+                jab++;
+            } else {
+                System.out.println("You throw a amazing cross slumping you oponent");
+                cross++;
+            }
+        }
+
+        if (secondChoice == "2") {
+            cross++;
+            System.out.print("You throw a great cross Knocking out you opponent");
+        } else {
+            System.out.print("You throw a great Kick puting you opponent to sleep");
+            kick++;
+        }
     }
 }
